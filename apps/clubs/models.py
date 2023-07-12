aa6f836aa6538a885f8cf713dbebee366f6b97c2
Lisandro Saavedra <saavedralisandro90@gmail.com>
2023-07-12 15:03:32 -0400

    Update category and clubs_2.html
    created category list drop down, and grouped the clubs by category in the html

--- apps/clubs/models.py
+++ apps/clubs/models.py
@@ -2,6 +2,17 @@ from django.conf import settings
 from django.db import models
 
 class Club(models.Model):
+    CATEGORY_CHOICES = (
+        ('Academic', 'Academic Clubs'),
+        ('Creative', 'Creative Clubs'),
+        ('Diversity/ Multicutural', 'Diversity Clubs'),
+        ('Professional', 'Professional Clubs'),
+        ('Religious', 'Religious Clubs'),
+        ('Social Service', 'Social Service Clubs'),
+        ('Sports', 'Sports Clubs'),
+    )
+    
+
     name = models.CharField(max_length=100, verbose_name='Club Name')
     email = models.EmailField(verbose_name='Club Email')
     meeting_type = models.CharField(max_length=100, default='On Campus', verbose_name='Hybrid ( On Campus & Virtual )')
@@ -10,7 +21,7 @@ class Club(models.Model):
     advisor_email = models.EmailField(verbose_name='Advisor Email')
     club_room = models.CharField(max_length=100, verbose_name='Club Room')
     active = models.CharField(max_length=100, default='Active')
-    category = models.CharField(max_length=100, verbose_name='Category')
+    category = models.CharField(max_length=255, choices=CATEGORY_CHOICES)
     club_members = models.IntegerField(default=0, verbose_name='Number of Members')
 
     def __str__(self):
