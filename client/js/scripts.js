3a9e4cdb8d4f70c59c36b6b4cb906b906b8c24a6
Sandrieli13 <tskitishvilisandro@gmail.com>
2023-07-18 20:40:26 -0400

    Navbar dumped
    Navbar replaced

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,22 +1,31 @@
-// script.js
-document.getElementById('registration-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-  
-    // Get the form data
-    const formData = new FormData(this);
-  
-    // Make an AJAX request to the Django backend
-    fetch('/register/', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-      // Handle the response from the Django view
-      document.getElementById('result').innerHTML = data.message;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  });
-  
+
+const navbar = document.getElementById("navbar");
+const navbarToggle = navbar.querySelector(".navbar-toggle");
+
+function openMobileNavbar() {
+  navbar.classList.add("opened");
+  navbarToggle.setAttribute("aria-expanded", "true");
+}
+
+function closeMobileNavbar() {
+  navbar.classList.remove("opened");
+  navbarToggle.setAttribute("aria-expanded", "false");
+}
+
+navbarToggle.addEventListener("click", () => {
+  if (navbar.classList.contains("opened")) {
+    closeMobileNavbar();
+  } else {
+    openMobileNavbar();
+  }
+});
+
+const navbarMenu = navbar.querySelector("#navbar-menu");
+const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+navbarLinksContainer.addEventListener("click", (clickEvent) => {
+  clickEvent.stopPropagation();
+});
+
+navbarMenu.addEventListener("click", closeMobileNavbar);
+
